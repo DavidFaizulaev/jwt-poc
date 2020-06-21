d4341b197229cb2c8831da7057a57525913bfd20
Dina Yakovlev <dina.yakovlev@zooz.com>
2020-06-21 09:04:35 +0000

    feat: add kong configurations

---
+++ config/env/kong/deploy-qa-kong.sh
@@ -0,0 +1,4 @@
+# Deployment
+echo Loading Deploy qa stage environment variables
+# Kong configuration variables
+export KONG_CONFIG_SECRET_PROJECT=svckongaccountsgateways
---
+++ config/local/runLocal.sh
@@ -0,0 +1,2 @@
+APP_URL_INTERNAL=http://91df11686874.ngrok.io BRANCH_NAME=dina ./config/local/deploy-local-qa-with-kong.sh
+###
